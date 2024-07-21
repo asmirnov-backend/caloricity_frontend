426f344b48c743cf1bd7084fcd3b8cb4cb94159d
Andrew Smirnov <smirnov.andrew.1999@yandex.ru>
2024-07-21 09:09:46 +0300

    Fix link

--- src/components/Actions/EditAction.tsx
+++ src/components/Actions/EditAction.tsx
@@ -1,14 +1,15 @@
 import { Tooltip } from "@nextui-org/react";
 import { Pencil } from "lucide-react";
 import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 export const EditAction = ({ id }: { id: string }) => {
   const pathname = usePathname();
   return (
-    <Tooltip content="Удалить" color="danger">
-      <a href={`${pathname}/edit/${id}`}>
+    <Tooltip content="Редактировать" color="warning">
+      <Link href={`${pathname}/edit/${id}`}>
         <Pencil fill="#979797" />
-      </a>
+      </Link>
     </Tooltip>
   );
 };
